e416fa7fdaf4ba54473040d2a6d8754fd08887a9
doncsoo <nanoxs10@gmail.com>
2020-08-02 12:13:18 +0200

    registration feature frontend

--- src/loginmodal.js
+++ src/loginmodal.js
@@ -4,7 +4,7 @@ import './App.css';
 
 class LogInModal extends Component
 {
-  state = {}
+  state = {mode: "login"}
 
   render()
   {
@@ -12,29 +12,53 @@ class LogInModal extends Component
         <div>
         <div id="id01" className="modal">
         <span onClick={() => this.props.app_parent.deRenderLogInModal()} className="close" title="Close Modal">Close</span>
-      
+         <div id="notifications"></div>
         <div className="modal-content animate">
-          <div className="logincontainer">
-            <label for="uname"><b>Username</b></label>
-            <input type="text" placeholder="Enter Username" name="uname" id="uname" required/>
-      
-            <label for="psw"><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" name="psw" id="psw" required/>
-      
-            <button onClick={() => this.attemptLogIn()} type="submit">Login</button>
-            <label>
-              <input type="checkbox" checked="checked" name="remember"/> Remember me
-            </label>
-          </div>
-      
+          {this.getMainContent()}
+
           <div className="logincontainer" style={{backgroundColor:"#f1f1f1"}}>
             <button onClick={() => this.props.app_parent.deRenderLogInModal()} className="button" class="cancelbtn">Exit</button>
-            <span className="psw">Forgot <a href="#">password?</a></span>
           </div>
         </div>
       </div>
       </div>
       )
+  }
+
+  getMainContent()
+  {
+    if(this.state.mode == "login")
+    {
+      return (<div id="login" className="logincontainer">
+      <label for="uname"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="uname" id="uname" required/>
+
+      <label for="psw"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" id="psw" required/>
+
+      <button onClick={() => this.attemptLogIn()} type="submit">Login</button>
+      <label>
+        <input type="checkbox" checked="checked" name="remember"/> Remember me
+      </label>
+      <button onClick={() => this.setState({mode: "register"})} className="button" class="cancelbtn">Exit</button>
+      <span className="psw">Forgot <a href="#">password?</a></span>
+    </div>);
+    }
+    else if(this.state.mode == "register")
+    {
+      return (<div id="register" className="logincontainer">
+      <label for="rname"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="rname" id="rname" required/>
+
+      <label for="rpsw"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="rpsw" id="rpsw" required/>
+
+      <label for="fname"><b>Full Name</b></label>
+      <input type="text" placeholder="Enter Password" name="fname" id="fname" required/>
+
+      <button onClick={() => this.attemptRegister()} type="submit">Register</button>
+    </div>);
+    }
   }
 
   componentDidMount()
@@ -56,6 +80,24 @@ class LogInModal extends Component
     if(resp.includes("User verified!")){
         this.props.app_parent.setUser(document.getElementById("uname").value);
         this.props.app_parent.deRenderLogInModal();
+    }
+  }
+
+  async attemptRegister()
+  {
+    var resp = await fetch("https://interact-server.herokuapp.com/register",{
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({username: document.getElementById("rname").value,
+                   password: document.getElementById("rpsw").value,
+                   fullname: document.getElementById("fname").value})
+        })
+        .then(r => r.text());
+    document.getElementById("notifications").innerHTML = "<h4>"+ resp +"</h4>"
+    if(resp.includes("Registration successful")){
+      this.setState({mode: "login"});
     }
   }
 }
